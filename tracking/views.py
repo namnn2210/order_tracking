735f52db0eb983cee64695eaf780da38ff8274ea
nqtofficial <132586004+nqtofficial@users.noreply.github.com>
2024-06-11 01:31:12 +0900

    Update views.py

--- tracking/views.py
+++ tracking/views.py
@@ -5,32 +5,32 @@ from bs4 import BeautifulSoup
 # Create your views here.
 def index(request):
     carriers = {
-         'cjkorex': 'CJ 대한통운',
-         'hanjin': '한진택배',
-         'ilogen': '로젠택배',
-         'kdexp': '경동택배',
-         'fedexkr': 'FedEx',
-         'ilyanglogis': '일양로지스',
-         'epost': '우체국택배',
-         'ems': 'EMS',
-         'chunil': '천일택배',
-         'kunyoung': '건영택배',
-         'ds3211': '대신택배'       
+        'ilyanglogis': 't1',
+        'epost': 't2',
+        'cjkorex': 't3',
+        'hanjin': 'ha',
+        'ilogen': 'iln',
+        'ems': 'ms',
+        'chunil': 'cnil',
+        'kunyoung': 'young',
+        'fedexkr': 'exkr',
+        'ds3211': '211',
+        'kdexp': 'exp'
     }
     
     display_carriers = list(carriers.values())
     results = []
     status_count = {
-        'Đã thông quan': 0,
-        'Đang thông quan': 0,
-        'Đang vận chuyển': 0,
-        'Đã giao hàng': 0,
+        'Đã_thông_quan': 0,
+        'Đang_thông_quan': 0,
+        'Đang_vận_chuyển': 0,
+        'Đã_giao_hàng': 0,
     }
     status_lists = {
-        'Đã thông quan': [],
-        'Đang thông quan': [],
-        'Đang vận chuyển': [],
-        'Đã giao hàng': [],
+        'Đã_thông_quan': [],
+        'Đang_thông_quan': [],
+        'Đang_vận_chuyển': [],
+        'Đã_giao_hàng': [],
     }
     
     if request.method == 'POST':
@@ -57,17 +57,17 @@ def index(request):
                 
                 # Increment status count and add to status list
                 if status_text == 'Đã thông quan':
-                    status_count['Đã thông quan'] += 1
-                    status_lists['Đã thông quan'].append(number)
+                    status_count['Đã_thông_quan'] += 1
+                    status_lists['Đã_thông_quan'].append(number)
                 elif status_text == 'Đang thông quan':
-                    status_count['Đang thông quan'] += 1
-                    status_lists['Đang thông quan'].append(number)
+                    status_count['Đang_thông_quan'] += 1
+                    status_lists['Đang_thông_quan'].append(number)
                 elif status_text == 'Đang vận chuyển':
-                    status_count['Đang vận chuyển'] += 1
-                    status_lists['Đang vận chuyển'].append(number)
+                    status_count['Đang_vận_chuyển'] += 1
+                    status_lists['Đang_vận_chuyển'].append(number)
                 elif status_text == 'Đã giao hàng':
-                    status_count['Đã giao hàng'] += 1
-                    status_lists['Đã giao hàng'].append(number)
+                    status_count['Đã_giao_hàng'] += 1
+                    status_lists['Đã_giao_hàng'].append(number)
         
         return render(request, 'index.html', {
             'results': results,
